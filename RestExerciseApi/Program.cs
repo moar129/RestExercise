e6fee1116bd4659ad1c5032a8e7f1ef53e0d3f0e
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-10-21 17:52:22 +0200

    Add CORS and Swagger support in Program.cs
    Updated Program.cs to configure CORS for all origins, methods, and headers. Added Swagger services and middleware for API documentation. Registered a singleton instance of IGamingTeamRepo<GamingTeam>.
    
    Updated RestExerciseApi.csproj to upgrade Microsoft.AspNetCore.OpenApi to version 9.0.10 and added Swashbuckle.AspNetCore version 9.0.6 for enhanced OpenAPI support.

--- RestExerciseApi/Program.cs
+++ RestExerciseApi/Program.cs
@@ -4,6 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader();
+                              });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IGamingTeamRepo<GamingTeam>>(new GamingTeamRepo());
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -18,7 +32,12 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseAuthorization();
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
